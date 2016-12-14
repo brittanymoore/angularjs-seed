0cee29d5e8b5e4e58c26a1cc9b18b889d3dbc14f
Brittany Moore <BZMK0C@NAM.corp.gm.com>
2016-12-14 10:49:16 -0500

    Removed old example artifacts.

--- src/app/mock/mock.module.js
+++ src/app/mock/mock.module.js
@@ -1,12 +1,10 @@
 import MockService from './mock.service';
-import ChatMockData from './models/chat.mock';
 import ToDoMockData from './models/todo.mock';
 import 'angular-mocks/angular-mocks';
 
 const MODULE_NAME = 'appMock';
 
 angular.module(MODULE_NAME, [ 'ngMockE2E' ])
-    .service('ChatMockData', ChatMockData)
     .service('ToDoMockData', ToDoMockData)
     .run( MockService );
 
--- src/app/mock/mock.service.js
+++ src/app/mock/mock.service.js
@@ -1,24 +1,12 @@
-var MockService = function ($httpBackend, ChatMockData, ToDoMockData) {
+var MockService = function ($httpBackend, ToDoMockData) { 
 
-    // mock get messages (SharePoint REST API)
-    $httpBackend.whenGET(/\/_api\/web\/lists\/getByTitle\(\'ChatMessages\'\)/).respond(function (method, url, data) {
-        var returnData = buildRESTResponse(ChatMockData.getMessages(data));
-        return [200, returnData, {}];
-    });
-
-    // mock add messages (SharePoint REST API)
-    $httpBackend.whenPOST(/\/_api\/web\/lists\/getByTitle\(\'ChatMessages\'\)/).respond(function (method, url, data) {
-        var returnData = buildRESTResponse(ChatMockData.addMessage(data));
-        return [200, returnData, {}];
-    });  
-
-    // mock get messages (SharePoint REST API)
+    // mock get tasks (SharePoint REST API)
     $httpBackend.whenGET(/\/_api\/web\/lists\/getByTitle\(\'Tasks\'\)/).respond(function (method, url, data) {
         var returnData = buildRESTResponse(ToDoMockData.getTasks(data));
         return [200, returnData, {}];
     });
 
-    // mock add messages (SharePoint REST API)
+    // mock add tasks (SharePoint REST API)
     $httpBackend.whenPOST(/\/_api\/web\/lists\/getByTitle\(\'Tasks\'\)/).respond(function (method, url, data) {
         let dataObj = JSON.parse(data);
         var returnData = buildRESTResponse(ToDoMockData.addTask(dataObj.Name));
