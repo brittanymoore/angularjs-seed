a9cc2d32406598c07fa245e4bd9819d6e0b671a6
Brittany Moore <moore.brittanyann@gmail.com>
2017-01-18 11:50:08 -0500

    Added more detailed e2e tests.

--- src/app/todo/todo.e2e.js
+++ src/app/todo/todo.e2e.js
@@ -1,23 +1,51 @@
-describe('E2E: ToDo:', () => {
+describe("E2E: ToDo:", () => {
 
-    it("Save button should be disabled when task field is empty.", () => {
+    let nameInput, addButton;
+
+    beforeEach(() => {
 
         // After landing on home page, click link to ToDo page.
         browser.get(browser.baseUrl);
         element(by.id('todoRoute')).click();
 
         // Get relevant form controls.
-        let nameInput = element(by.model('vm.name'));
-        let submitButton = element(by.id('submit'));
+        nameInput = element(by.model('vm.name'));
+        addButton = element(by.id('submit'));        
 
-        // Submit button by default should be disabled.
+    });
+
+    it("Should disable add button when task field is empty.", () => {
+
         expect(nameInput.getAttribute('value')).toBe('');
-        expect(submitButton.isEnabled()).toBe(false);
+        expect(addButton.isEnabled()).toBe(false);
 
-        // After entering some keys in the name field, submit button should be enabled.
+    });
+
+    it("Should enable add button when task field contains a value.", () => {
+
         nameInput.sendKeys('test task');
         expect(nameInput.getAttribute('value')).toBe('test task');
-        expect(submitButton.isEnabled()).toBe(true);
+        expect(addButton.isEnabled()).toBe(true);
+
+    });
+
+    it("Should add new task and clear form after add button clicked.", () => {
+
+        // There should be two mock tasks initially.
+        let tasks = element.all(by.css('#taskList > li'));
+        expect(tasks.count()).toEqual(2);
+
+        // Provide a name and click Add.
+        nameInput.sendKeys('test task');
+        addButton.click();
+
+        // Wait for click response to complete.
+        browser.waitForAngular();
+
+        // Verify page state.
+        tasks = element.all(by.css('#taskList > li'));
+        expect(tasks.count()).toEqual(3);
+        expect(nameInput.getAttribute('value')).toBe('');
 
     });
 
